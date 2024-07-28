382b0627c171f963e0158145b4a2b32edaa2f92b
emersonfsti <emersonfsti@gmail.com>
2024-07-28 17:13:21 -0300

    commit002desafio1

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -18,3 +18,18 @@ resultado = primeira_entrada + " " + segunda_entrada
 
 # Exibindo o resultado
 print("A string concatenada é:", resultado)
+
+
+# ============================================================
+# Versão do mesmo codigo com opção de entrada de dados:
+# ============================================================
+
+# Recebendo os dados do usuário
+primeira_entrada = input("Digite a primeira frase: ")
+segunda_entrada = input("Digite a segunda frase: ")
+
+# Concatenando as duas entradas
+resultado = primeira_entrada + " " + segunda_entrada
+
+# Exibindo o resultado
+print("A string concatenada é:", resultado)
---
+++ resolucoes_code/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+# Recebendo os dados do usuário
+primeira_entrada = input("Digite a primeira frase: ")
+segunda_entrada = input("Digite a segunda frase: ")
+
+# Concatenando as duas entradas
+resultado = primeira_entrada + " " + segunda_entrada
+
+# Exibindo o resultado
+print("A string concatenada é:", resultado)
